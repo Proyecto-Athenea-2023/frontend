c927c45d1d9066de769672c25666509e0abe6354
EngAndres <jerarquia04@gmail.com>
2023-04-18 07:31:35 -0500

    Add update and delete both JS functions and HTML buttons

--- functions_js.js
+++ functions_js.js
@@ -8,6 +8,7 @@ function printResponse(items){
 		table += "<td>" + items[i].id_category + "</td>";
 		table += "<td>" + items[i].brand + "</td>";
 		table += "<td>" + items[i].year + "</td>";
+		table += "<td><button onclick='deleteCostume(" + items[i].id_costume + ")'>Eliminar</button></td>";
 
 		table += "</tr>"
 	}
@@ -21,6 +22,7 @@ function getCostumes(){
 		type: "GET",
 		datatype: "JSON",
 		success: function(response){
+			$("#result").empty();
 			printResponse(response.items);
 		},
 		error: function(xhr, status){
@@ -32,5 +34,87 @@ function getCostumes(){
 }
 
 function saveCostume(){
+	let data = {
+		id: $("#costume_id").val(),
+		name: $("#costume_name").val(),
+		brand: $("#costume_brand").val(),
+		model: $("#costume_year").val(),
+		category: $("#costume_category").val()
+	}
+	
+	data = JSON.stringify(data);
+	$.ajax({
+		url: "https://g05efb35d3eef7e-customesdb.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/api/costume",
+		type: "POST",
+		data: data,
+		contentType: "application/JSON",
+		datatype: "JSON",
+		success: function(response){
+			$("#result").empty();
+			alert("El disfraz se ha guardado correctamente.");
+			$("#costume_id").val("");
+			$("#costume_name").val("");
+			$("#costume_brand").val("");
+			$("#costume_year").val("");
+			$("#costume_category").val("");
 
+			getCostumes();
+		},
+		error: function(xhr, status){
+			alert("Hubo un problema agregando el nuevo disfraz. Intente de nuevo mas tarde.");
+			console.log(xhr)
+		}
+	});
+}
+
+
+function updateCostume(){
+	let data = {
+		id: $("#costume_id").val(),
+		name: $("#costume_name").val(),
+		description: "Actualizado"
+	}
+	
+	data = JSON.stringify(data);
+	$.ajax({
+		url: "https://g05efb35d3eef7e-customesdb.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/api/costume",
+		type: "PUT",
+		data: data,
+		contentType: "application/JSON",
+		datatype: "JSON",
+		success: function(response){
+			$("#result").empty();
+			alert("El disfraz se ha actualizado correctamente.");
+			$("#costume_id").val("");
+			$("#costume_name").val("");
+			$("#costume_brand").val("");
+			$("#costume_year").val("");
+			$("#costume_category").val("");
+
+			getCostumes();
+		},
+		error: function(xhr, status){
+			alert("Hubo un problema actualizando el nuevo disfraz. Intente de nuevo mas tarde.");
+			console.log(xhr)
+		}
+	});
+}
+
+
+function deleteCostume(costume_id){
+	$.ajax({
+		url: "https://g05efb35d3eef7e-customesdb.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/api/costume?id="+costume_id,
+		type: "DELETE", 
+		datatype: "JSON",
+		success: function(response){
+			$("#result").empty();
+			alert("El disfraz se ha eliminado correctamente.");
+		
+			getCostumes();
+		},
+		error: function(xhr, status){
+			alert("Hubo un problema eliminando el nuevo disfraz. Intente de nuevo mas tarde.");
+			console.log(xhr)
+		}		
+	});
 }
